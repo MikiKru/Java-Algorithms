c1615127b476a63d5c32756a4bcad3b270ca91bc
MikiKru <michal_kruczkowski@o2.pl>
2019-02-20 11:45:30 +0100

    Algorithms z1 al

--- src/Z1/Algorithms1.java
+++ src/Z1/Algorithms1.java
@@ -14,9 +14,27 @@ public class Algorithms1 {
     // algorytm silnia z n
     public int factorial(int n){
         // 2
-
+        int result = 1;
         // 3
-        
-        return
+        for(int i = n; i > 1; i--){
+            result = result * i;
+        }
+        return result;
+    }
+    // algorytm ciągu geometrycznego
+    public int geoSequence(int a0, int q, int n){
+        int sum = 0;
+        for(int i = 0; i <= n-1; i++){
+            sum = sum + (a0 * power(q, i));
+        }
+        return sum;
+    }
+    // algorytm ciągu arytmetycznego
+    public int algSequence(int a0, int r, int n){
+        int sum = 0;
+        for(int i = 0; i <= n-1; i++){
+            sum = sum + (a0 + i*r);
+        }
+        return sum;
     }
 }
--- src/Z1/Main.java
+++ src/Z1/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Algorithms1 a = new Algorithms1();
         System.out.println("Wynik potęgowania: " + a.power(3,4));
+        System.out.println("Wynik silni: " + a.factorial(4));
+        System.out.println("Suma cg: " + a.geoSequence(2,2,4));
+        System.out.println("Suma ca: " + a.algSequence(2,2,4));
     }
 }
