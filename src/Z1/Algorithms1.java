368339bf79ce391e5ff9a8ddf3fd76a0d5722454
MikiKru <michal_kruczkowski@o2.pl>
2019-02-20 11:01:28 +0100

    Algorithms z1 power

---
+++ src/Z1/Algorithms1.java
@@ -0,0 +1,22 @@
+package Z1;
+
+public class Algorithms1 {
+    // algorytm potęgowania
+    public int power(int x, int y){
+        // 2
+        int result = 1;
+        // 3
+        for(int i = 1; i <= y; i++ ){
+            result = result * x;
+        }
+        return result;
+    }
+    // algorytm silnia z n
+    public int factorial(int n){
+        // 2
+
+        // 3
+        
+        return
+    }
+}
---
+++ src/Z1/Main.java
@@ -0,0 +1,8 @@
+package Z1;
+
+public class Main {
+    public static void main(String[] args) {
+        Algorithms1 a = new Algorithms1();
+        System.out.println("Wynik potęgowania: " + a.power(3,4));
+    }
+}
